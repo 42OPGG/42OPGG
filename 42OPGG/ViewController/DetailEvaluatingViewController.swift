19e0e8f38870d8bf177cbdc856d55581a6c3b3e2
ChoiKanghun <rkdgns4562@naver.com>
2021-02-09 17:59:52 +0900

    Add: loading image

--- 42OPGG/ViewController/DetailEvaluatingViewController.swift
+++ 42OPGG/ViewController/DetailEvaluatingViewController.swift
@@ -14,6 +14,41 @@ class DetailEvaluatingViewController: UIViewController {
 
     var correctorLogs: [CorrectorLog] = []
 
+    lazy var activityIndicator: UIActivityIndicatorView = {
+        // Create an indicator.
+        let activityIndicator = UIActivityIndicatorView()
+        activityIndicator.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        activityIndicator.center = self.view.center
+        activityIndicator.color = UIColor.red
+        // Also show the indicator even when the animation is stopped.
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.style = UIActivityIndicatorView.Style.large
+        // Start animation.
+        activityIndicator.stopAnimating()
+        return activityIndicator
+    }()
+    
+    // MARK:- 토스트메시지 출력 함수.
+    func showToast(message : String) {
+        let width_variable:CGFloat = 10
+        let toastLabel = UILabel(frame: CGRect(x: width_variable, y:self.view.frame.size.height-100, width:view.frame.size.width-2*width_variable, height: 35))
+        // 뷰가 위치할 위치를 지정해준다. 여기서는 아래로부터 100만큼 떨어져있고, 너비는 양쪽에10만큼 여백을 가지며, 높이는 35로
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.textAlignment = .center;
+        toastLabel.font = UIFont(name: "Montserrat-Light", size: 12.0)
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 4.0, delay: 0.1, options:.curveEaseOut, animations: {
+            toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
+    }
+    
     @IBAction func touchUpBackButton(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
@@ -25,6 +60,9 @@ class DetailEvaluatingViewController: UIViewController {
         self.logTableView.dataSource = self
         
         self.logTableView.backgroundColor = UIColor(patternImage: UIImage(named: "gon_cover_resize")!)
+        
+        // add loading code
+        self.view.addSubview(self.activityIndicator)
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -33,7 +71,10 @@ class DetailEvaluatingViewController: UIViewController {
         guard let intraId = UserInformation.shared.id
             else {return}
         
-        guard let getEvaluatingAPIurl: URL = URL(string: "https://api.jiduckche.com/api/corrector/" + intraId + "/15")
+        // show loading image
+        self.activityIndicator.startAnimating()
+        
+        guard let getEvaluatingAPIurl: URL = URL(string: "https://opggapi.herokuapp.com/api/corrector/" + intraId + "/15")
             else {return}
     
             let getEvaluatingAPISession: URLSession = URLSession(configuration: .default)
@@ -41,6 +82,8 @@ class DetailEvaluatingViewController: UIViewController {
                 (data: Data?, response: URLResponse?, error: Error?) in
                 if let error = error {
                     print(error.localizedDescription)
+                    // unshow loading image
+                    self.activityIndicator.stopAnimating()
                     return
                 }
                 guard let data = data
@@ -50,10 +93,17 @@ class DetailEvaluatingViewController: UIViewController {
                         JSONDecoder().decode(CorrectorAPIResponse.self, from: data)
                     self.correctorLogs = apiResponse.data
                     DispatchQueue.main.async {
+                        // unshow loading image
+                        self.activityIndicator.stopAnimating()
+                        if self.correctorLogs.count == 0 {
+                            self.showToast(message: "최근 평가해준 항목이 없습니다.")
+                        }
                         self.logTableView.reloadData()
                     }
                 } catch (let err) {
                     print(err.localizedDescription)
+                    // unshow loading image
+                    self.activityIndicator.stopAnimating()
                 }
             }
         getEvaluatingAPIDataTask.resume()
